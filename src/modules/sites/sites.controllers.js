f07c38070f5a71aab18f175f865c5f82461dcce8
walterlul <walterjobs44@gmail.com>
2023-10-12 09:56:52 -0300

    se termino el modulo sitios, agregado de crud

--- src/modules/sites/sites.controllers.js
+++ src/modules/sites/sites.controllers.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { validarJwt } from '../../helpers/validar-jwt.js'
-import { createSite, createSiteGroupMember, createSiteMember, deleteSite, getContainerDocumentLibrary, getMySites, getSites } from './sites.service.js'
+import { createSite, createSiteMember, deleteSite, deleteSiteMember, getContainerDocumentLibrary, getMySites, getOneSite, getOneSiteMember, getSiteMembers, getSites, updateSite, updateSiteMember } from './sites.service.js'
 const router = Router()
 
 // GET
@@ -9,6 +9,17 @@ router.get('/all', validarJwt, async (req, res) => {
     const ticket = req.ticket
     const sites = await getSites({ ticket })
     return res.status(sites.status).json(sites)
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
+router.get('/:id', validarJwt, async (req, res) => {
+  try {
+    const ticket = req.ticket
+    const idSite = req.params.id
+    const site = await getOneSite({ ticket, idSite })
+    return res.status(site.status).json(site)
   } catch (error) {
     return res.status(500).json(error)
   }
@@ -31,12 +42,32 @@ router.post('/create', validarJwt, async (req, res) => {
   }
 })
 
+// UPDATE
+router.put('/update/:id', validarJwt, async (req, res) => {
+  try {
+    const idSite = req.params.id
+    console.log(idSite)
+    const { title, description, visibility } = req.body
+    const ticket = req.ticket
+    const updatedSite = await updateSite({
+      ticket,
+      idSite,
+      siteData: {
+        title, description, visibility
+      }
+    })
+    return res.status(updatedSite.status).json(updatedSite)
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
 // DELETE
 
-router.delete('/delete', validarJwt, async (req, res) => {
+router.delete('/delete/:id', validarJwt, async (req, res) => {
   try {
     const ticket = req.ticket
-    const idSite = req.params
+    const idSite = req.params.id
     const deletedSite = await deleteSite({ ticket, idSite })
     return res.status(deletedSite.status).json(deletedSite)
   } catch (error) {
@@ -44,7 +75,42 @@ router.delete('/delete', validarJwt, async (req, res) => {
   }
 })
 
-// el id corresponde al sitio donde sera agregada la persona y se le da un rol en el mismo
+// CRUD DE MIEMBROS DEL SITIO
+
+// GET
+router.get('/all-members/:id', validarJwt, async (req, res) => {
+  try {
+    const ticket = req.ticket
+    const idSite = req.params.id
+    const siteMembers = await getSiteMembers({
+      ticket,
+      idSite
+    })
+    return res.status(siteMembers.status).json(siteMembers)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json(error)
+  }
+})
+
+router.get('/one-member/:id/:idPerson', validarJwt, async (req, res) => {
+  try {
+    const ticket = req.ticket
+    const idSite = req.params.id
+    const idPerson = req.params.idPerson
+    const siteMember = await getOneSiteMember({
+      ticket,
+      idSite,
+      idPerson
+    })
+    return res.status(siteMember.status).json(siteMember)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json(error)
+  }
+})
+
+// POST
 router.post('/create-member/:id', validarJwt, async (req, res) => {
   try {
     const ticket = req.ticket
@@ -64,25 +130,49 @@ router.post('/create-member/:id', validarJwt, async (req, res) => {
   }
 })
 
-// el id del body corresponde al id del grupo
-router.post('/create-group-member/:id', validarJwt, async (req, res) => {
+// UPDATE
+router.put('/update-member/:id/:idPerson', validarJwt, async (req, res) => {
   try {
     const ticket = req.ticket
     const idSite = req.params.id
-    console.log(idSite)
-    const groupData = req.body
-    const siteGroupMember = await createSiteGroupMember({
+    const idPerson = req.params.idPerson
+
+    const { role } = req.body
+    const updatedSiteMember = await updateSiteMember({
       ticket,
       idSite,
-      groupData
+      idPerson,
+      personData: {
+        role
+      }
     })
-    return res.json(siteGroupMember)
+    return res.status(updatedSiteMember.status).json(updatedSiteMember)
   } catch (error) {
     console.error(error)
     return res.status(500).json(error)
   }
 })
 
+// DELETE
+router.delete('/delete-member/:id/:idPerson', validarJwt, async (req, res) => {
+  try {
+    const ticket = req.ticket
+    const idSite = req.params.id
+    const idPerson = req.params.idPerson
+
+    const deletedSiteMember = await deleteSiteMember({
+      ticket,
+      idSite,
+      idPerson
+    })
+    return res.status(deletedSiteMember.status).json(deletedSiteMember)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json(error)
+  }
+})
+
+// ENDPOINTS AUXILIARES
 router.get('/getContainerSite/:siteName', validarJwt, async (req, res) => {
   const siteName = req.params.siteName
   const ticket = req.ticket
