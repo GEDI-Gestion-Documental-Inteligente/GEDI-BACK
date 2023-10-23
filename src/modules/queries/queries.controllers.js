51d59c6206c03923ff583ed3297f299b869c6813
Florcita <florvllb33@gmailcom>
2023-10-23 16:13:07 -0300

    Se ha corregido el llamado de los servicios y tambien la codificacion de los terminos

--- src/modules/queries/queries.controllers.js
+++ src/modules/queries/queries.controllers.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { validarJwt } from '../../helpers/validar-jwt.js'
-import { sendQueriesForNodes, sendQueriesWithTerm } from './queries.services.js'
+import { sendQueriesForNodes, sendQueriesForPeople, sendQueriesForSites, sendQueriesWithTerm } from './queries.services.js'
 
 const router = Router()
 
@@ -34,7 +34,7 @@ router.get('/searchSites', validarJwt, async (req, res) => {
   const term = req.query
 
   try {
-    const resultQuery = await sendQueriesForNodes({ ticket, term })
+    const resultQuery = await sendQueriesForSites({ ticket, term })
     return res.status(resultQuery.status).json(resultQuery)
   } catch (error) {
     return res.status(500).json(error)
@@ -43,10 +43,10 @@ router.get('/searchSites', validarJwt, async (req, res) => {
 
 router.get('/searchPeople', validarJwt, async (req, res) => {
   const ticket = req.ticket
-  const term = req.query
+  const { term } = req.query
 
   try {
-    const resultQuery = await sendQueriesForNodes({ ticket, term })
+    const resultQuery = await sendQueriesForPeople({ ticket, term })
     return res.status(resultQuery.status).json(resultQuery)
   } catch (error) {
     return res.status(500).json(error)
